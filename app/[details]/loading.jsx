87350643f81d46ff75ca71c148c746ed764b175c
Khayam Ijaz <ijazkhayam@gmail.com>
2024-12-30 09:34:04 +0500

    update

--- app/[details]/loading.jsx
+++ app/[details]/loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
   return (
     <>
-          <div className="w-full h-screen flex items-center justify-center">
+          <div className="w-full h-screen flex bg-black text-white items-center justify-center">
               <h1 className='text-4xl'>Loding...</h1>
       </div>
     </>
